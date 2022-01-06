feed045227edbce91248e98de121a4429a2e5868
buzhifanji <buzhifanji@163.com>
2022-01-07 00:12:30 +0800

    rollup 打包多个组件包 初步跑通

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,30 @@
 import path from 'path'
-import resolve from 'rollup-plugin-node-resolve' // 依赖引用插件
+const fs = require('fs-extra')
+import nodeResolve from 'rollup-plugin-node-resolve' // 依赖引用插件
 import commonjs from 'rollup-plugin-commonjs' // commonjs模块转换插件
 import { eslint } from 'rollup-plugin-eslint' // eslint插件
 import ts from 'rollup-plugin-typescript2'
-const getPath = _path => path.resolve(__dirname, _path)
-import packageJSON from './package.json'
 
+if (!process.env.TARGET) {
+    throw new Error('TARGET package must be specified via --environment flag.')
+}
 
-const extensions = [
-    '.js',
-    '.ts',
-    '.tsx'
-]
+const version = require('./package.json').version
+const tsconfig = path.resolve(__dirname, 'tsconfig.json')
 
+// process.env.TARGET 由 execa 传递过来，确定具体包名
+const packagesDir = path.resolve(__dirname, 'packages')
+const packageDir = path.resolve(packagesDir, process.env.TARGET)
+const resolve = p => path.resolve(packageDir, p)
+const pkg = require(resolve(`package.json`))
+const packageOptions = pkg.buildOptions || {}
+const name = packageOptions.filename || path.basename(packageDir)
+
+const extensions = ['.js', '.ts']
 
 // ts
 const tsPlugin = ts({
-    tsconfig: getPath('./tsconfig.json'), // 导入本地ts配置
+    tsconfig, // 导入本地ts配置
     extensions
 })
 
@@ -28,25 +36,29 @@ const esPlugin = eslint({
     exclude: ['node_modules/**', 'lib/**']
 })
 
-
 // 基础配置
 const commonConf = {
-    input: getPath('./packages/button/src/index.ts'),
+    input: resolve('src/index.ts'),
     plugins: [
-        resolve(extensions),
+        nodeResolve(extensions),
         commonjs(),
         esPlugin,
         tsPlugin,
-    ]
+    ],
+    output: {
+        name,
+        file: resolve(`dist/${name}.esm-bundler.js`),
+        format: 'es'
+    }
 }
 // 需要导出的模块类型
 const outputMap = [
     {
-        file: packageJSON.main, // 通用模块
+        file: path.resolve(__dirname, `dist/${name}.js`), // 通用模块
         format: 'umd',
     },
     {
-        file: packageJSON.module, // es6模块
+        file: path.resolve(__dirname, `dist/${name}.esm.js`), // es6模块
         format: 'es',
     }
 ]
@@ -54,5 +66,4 @@ const outputMap = [
 
 const buildConf = options => Object.assign({}, commonConf, options)
 
-
-export default outputMap.map(output => buildConf({ output: { name: packageJSON.name, ...output } }))
+export default outputMap.map(output => buildConf({ output: { name, ...output } }))
