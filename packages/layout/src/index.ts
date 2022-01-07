9b1cc1070d483f300691f3bc8344ba39e404ba55
buzhifanji <buzhifanji@163.com>
2022-01-07 23:52:38 +0800

    添加 layout 组件初步模型

---
+++ packages/layout/src/index.ts
@@ -0,0 +1,1 @@
+export { VLayout } from './layout'
---
+++ packages/layout/src/template.ts
@@ -0,0 +1,14 @@
+import { getTemplate } from "utils/components";
+
+export function renderLayoutTemplate(): HTMLTemplateElement {
+    const template = getTemplate();
+    template.innerHTML = `
+<style>
+    :host {}
+
+    ::slotted() {}
+</style>
+<slot></slot>
+`
+    return template
+}
