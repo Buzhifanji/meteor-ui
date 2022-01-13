e2d8320bef279903c365b0c73b6ea6a60752925a
buzhifanji <buzhifanji@163.com>
2022-01-14 00:13:58 +0800

    添加 v-grid

--- packages/grid/src/grid.ts
+++ packages/grid/src/grid.ts
@@ -1,26 +1,73 @@
-import { defineCustomElement } from "utils"
-import { renderGridTemplate } from "./template"
+import { defineCustomElement } from "utils";
+import { renderGridTemplate } from "./template";
 
+const XGAP = "x-gap";
+const YGAP = "y-gap";
+const COLS = "cols";
 
 export class VGrid extends HTMLElement {
-static get observedAttributes() { return [] }
-constructor() {
-super()
-this.render()
-}
-private render() {
-this.attachShadow({ mode: 'open' })
-const template = renderGridTemplate()
-this.shadowRoot!.appendChild(template.content.cloneNode(true));
+  static get observedAttributes() {
+    return [XGAP, YGAP, COLS];
+  }
+  constructor() {
+    super();
+    this.render();
+  }
+  get [XGAP]() {
+    return this.getGap(XGAP);
+  }
+  get [YGAP]() {
+    return this.getGap(YGAP);
+  }
+  /**
+   * 范围 1-24
+   */
+  get cols() {
+    return this.getAttribute(COLS) || 0;
+  }
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    if (oldValue === newValue) {
+      return;
+    }
+    switch (name) {
+      case XGAP:
+        this.updateXGap();
+        break;
+      case YGAP:
+        this.updateYGap();
+        break;
+      case COLS:
+        this.updateCols();
+        break;
+    }
+  }
+
+  private render() {
+    this.attachShadow({ mode: "open" });
+    const template = renderGridTemplate();
+    this.shadowRoot!.appendChild(template.content.cloneNode(true));
+  }
+  private getGap(key: "y-gap" | "x-gap") {
+    let gap = this.getAttribute(key);
+    if (gap) {
+      gap += "px";
+    } else {
+      gap = "0px";
+    }
+    return gap;
+  }
+  private updateYGap() {
+    this.style.setProperty("row-gap", this[YGAP]);
+  }
+  private updateXGap() {
+    this.style.setProperty("column-gap", this[XGAP]);
+  }
+  private updateCols() {
+    this.style.setProperty(
+      "grid-template-columns",
+      `repeat(${this.cols}, minmax(0px, 1fr))`
+    );
+  }
 }
 
-attributeChangedCallback(name: string, oldValue: string, newValue: string) {
-
-}
-connectedCallback() {
-
-}
-disconnectedCallback() {
-}
-}
-defineCustomElement('v-grid', VGrid )
+defineCustomElement("v-grid", VGrid);
--- packages/grid/src/template.ts
+++ packages/grid/src/template.ts
@@ -1,14 +1,31 @@
 import { getTemplate } from "utils/components";
 
 export function renderGridTemplate(): HTMLTemplateElement {
-const template = getTemplate();
-template.innerHTML = `
-<style>
-    :host {}
+  const template = getTemplate();
+  template.innerHTML = `
+        <style>
+            :host {
+                display:grid;
+                grid-template-columns:repeat(24,1fr);
+                grid-gap: 0;
+            }
+        </style>
+        <slot></slot>
+    `;
+  return template;
+}
 
-    ::slotted() {}
-</style>
-<slot></slot>
-`
-return template
+export function renderGridItemTemplate(): HTMLTemplateElement {
+  const template = getTemplate();
+  template.innerHTML = `
+        <style>
+            :host {
+                display:grid;
+                grid-template-columns:repeat(24,1fr);
+                grid-gap: 0;
+            }
+        </style>
+        <slot></slot>
+    `;
+  return template;
 }
