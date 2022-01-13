ffb3fbbe9c4c8fa672d565ac3b66d5ed25804615
buzhifanji <buzhifanji@163.com>
2022-01-13 10:50:53 +0800

    新增 v-space justify属性的测试用例

--- packages/space/test/space.test.ts
+++ packages/space/test/space.test.ts
@@ -15,7 +15,12 @@ describe("web component v-sapce", () => {
       <span>2</span>
       <span>3</span>
     </v-space>
-    <v-space class="v-space-slot-inline">
+    <v-space inline class="v-space-slot-inline">
+      <span>1</span>
+      <span>2</span>
+      <span>3</span>
+    </v-space>
+    <v-space class="v-space-slot-justify">
       <span>1</span>
       <span>2</span>
       <span>3</span>
@@ -57,5 +62,64 @@ describe("web component v-sapce", () => {
     }, 100);
   });
 
-  it("set inline property", () => {});
+  it("set inline property", () => {
+    const space = Selector(".v-space-slot-inline")!;
+    expect(space).not.toBeNull();
+
+    expect(getStylePropertyValue(space, "display")).toBe("inline-flex");
+    expect(getStylePropertyValue(space, "justify-content")).toBe("flex-start");
+    expect(getStylePropertyValue(space, "flex-direction")).toBe("row");
+    expect(getStylePropertyValue(space, "align-items")).toBe("stretch");
+    expect(getStylePropertyValue(space, "flex-wrap")).toBe("wrap");
+
+    space.setAttribute("inline", "false");
+    expect(getStylePropertyValue(space, "display")).toBe("flex");
+    expect(getStylePropertyValue(space, "flex-wrap")).toBe("wrap");
+
+    space.setAttribute("inline", "true");
+    expect(getStylePropertyValue(space, "display")).toBe("inline-flex");
+    expect(getStylePropertyValue(space, "flex-wrap")).toBe("wrap");
+  });
+
+  it("set justify property", () => {
+    const space = Selector(".v-space-slot-justify")!;
+    expect(space).not.toBeNull();
+    expect(getStylePropertyValue(space, "justify-content")).toBe("flex-start");
+    expect(getStylePropertyValue(space, "display")).toBe("flex");
+    expect(getStylePropertyValue(space, "flex-direction")).toBe("row");
+    expect(getStylePropertyValue(space, "align-items")).toBe("stretch");
+    expect(getStylePropertyValue(space, "flex-wrap")).toBe("wrap");
+
+    space.setAttribute("justify", "center");
+    expect(getStylePropertyValue(space, "justify-content")).toBe("center");
+    expect(getStylePropertyValue(space, "display")).toBe("flex");
+    expect(getStylePropertyValue(space, "flex-direction")).toBe("row");
+    expect(getStylePropertyValue(space, "align-items")).toBe("stretch");
+    expect(getStylePropertyValue(space, "flex-wrap")).toBe("wrap");
+
+    space.setAttribute("justify", "flex-end");
+    expect(getStylePropertyValue(space, "justify-content")).toBe("flex-end");
+    expect(getStylePropertyValue(space, "display")).toBe("flex");
+    expect(getStylePropertyValue(space, "flex-direction")).toBe("row");
+    expect(getStylePropertyValue(space, "align-items")).toBe("stretch");
+    expect(getStylePropertyValue(space, "flex-wrap")).toBe("wrap");
+
+    space.setAttribute("justify", "space-around");
+    expect(getStylePropertyValue(space, "justify-content")).toBe(
+      "space-around"
+    );
+    expect(getStylePropertyValue(space, "display")).toBe("flex");
+    expect(getStylePropertyValue(space, "flex-direction")).toBe("row");
+    expect(getStylePropertyValue(space, "align-items")).toBe("stretch");
+    expect(getStylePropertyValue(space, "flex-wrap")).toBe("wrap");
+
+    space.setAttribute("justify", "space-between");
+    expect(getStylePropertyValue(space, "justify-content")).toBe(
+      "space-between"
+    );
+    expect(getStylePropertyValue(space, "display")).toBe("flex");
+    expect(getStylePropertyValue(space, "flex-direction")).toBe("row");
+    expect(getStylePropertyValue(space, "align-items")).toBe("stretch");
+    expect(getStylePropertyValue(space, "flex-wrap")).toBe("wrap");
+  });
 });
