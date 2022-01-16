59369e63ef54b6393594c16ab115caa9d923fa48
buzhifanji <buzhifanji@163.com>
2022-01-16 14:40:44 +0800

    新增：v-text

--- packages/text/src/template.ts
+++ packages/text/src/template.ts
@@ -1,14 +1,49 @@
+import { getCssVariable } from "styles/cssVariable";
+import {
+  dangerTextColor,
+  fontSize,
+  infoTextColor,
+  primaryTextColor,
+  successTextColor,
+  textColor,
+  warningTextColor,
+} from "styles/cssVariable/variable";
 import { getTemplate } from "utils/components";
 
 export function renderTextTemplate(): HTMLTemplateElement {
-const template = getTemplate();
-template.innerHTML = `
-<style>
-    :host {}
-
-    ::slotted() {}
-</style>
-<slot></slot>
-`
-return template
+  const template = getTemplate();
+  template.innerHTML = `
+    <style>
+        :host {
+            font-size:${getCssVariable(fontSize)};
+            color: ${getCssVariable(textColor)};
+        }
+        :host([line-clamp]) {
+            display: -webkit-inline-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 1;
+            word-break: break-all;
+            text-size-adjust: 100%;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+        :host([color="primary"]) {
+          color: ${getCssVariable(primaryTextColor)};
+        }
+        :host([color="success"]) {
+          color: ${getCssVariable(successTextColor)};
+        }
+        :host([color="info"]) {
+          color: ${getCssVariable(infoTextColor)};
+        }
+        :host([color="warning"]) {
+          color: ${getCssVariable(warningTextColor)};
+        }
+        :host([color="danger"]) {
+          color: ${getCssVariable(dangerTextColor)};
+        }
+    </style>
+    <slot></slot>
+    `;
+  return template;
 }
--- packages/text/src/text.ts
+++ packages/text/src/text.ts
@@ -1,26 +1,56 @@
-import { defineCustomElement } from "utils"
-import { renderTextTemplate } from "./template"
+import { defineCustomElement } from "utils";
+import { renderTextTemplate } from "./template";
 
+const COLOR = "color";
+const ROW = "line-clamp";
 
 export class VText extends HTMLElement {
-static get observedAttributes() { return [] }
-constructor() {
-super()
-this.render()
-}
-private render() {
-this.attachShadow({ mode: 'open' })
-const template = renderTextTemplate()
-this.shadowRoot!.appendChild(template.content.cloneNode(true));
-}
+  static get observedAttributes() {
+    return [COLOR, ROW];
+  }
+  constructor() {
+    super();
+    this.render();
+  }
+  get color() {
+    return this.getAttribute(COLOR);
+  }
+  get [ROW]() {
+    return this.getAttribute(ROW);
+  }
 
-attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    if (oldValue === newValue) {
+      return;
+    }
 
+    switch (name) {
+      case ROW:
+        this.updateRow();
+        break;
+      case COLOR:
+        this.updateColor();
+        break;
+    }
+  }
+  connectedCallback() {}
+  disconnectedCallback() {}
+  private render() {
+    this.attachShadow({ mode: "open" });
+    const template = renderTextTemplate();
+    this.shadowRoot!.appendChild(template.content.cloneNode(true));
+  }
+  private updateRow() {
+    if (this[ROW]) {
+      this.style.setProperty("-webkit-line-clamp", this[ROW]);
+    }
+  }
+  private updateColor() {
+    const value = this.color;
+    const typeList = ["primary", "success", "info", "warning", "danger"];
+    if (value && !typeList.includes(value)) {
+      this.style.setProperty("color", value);
+    }
+  }
 }
-connectedCallback() {
-
-}
-disconnectedCallback() {
-}
-}
-defineCustomElement('v-text', VText )
+defineCustomElement("v-text", VText);
