7f88c0b6c43fe5602e2ac6687d7b720e76b7399a
buzhifanji <buzhifanji@163.com>
2022-01-16 14:49:34 +0800

    删除 css变量配置 旧代码

--- utils/style.ts
+++ utils/style.ts
@@ -1,19 +1,3 @@
-import { basicBackgroundColor, commonStyleConfig } from "styles/common/common";
-import { themeVariableName } from "styles/enums";
-
-/**
- * 获取 颜色
- * @param variableName
- * @returns
- */
-export function getBascBackgroundColor(variableName: themeVariableName) {
-  return `var(${variableName},${basicBackgroundColor[variableName]})`;
-}
-
-export function getStyleVarValue(variableName: themeVariableName) {
-  return `var(${variableName},${commonStyleConfig[variableName]})`;
-}
-
 /**
  * 支持 40 40px 40em 40rem; 默认 px
  * @param value
