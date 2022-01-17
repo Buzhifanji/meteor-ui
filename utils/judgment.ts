d0eff017d00bdb0a337d96d50c055ced9b96eeb9
buzhifanji <buzhifanji@163.com>
2022-01-17 22:23:01 +0800

    新增 属性判断 公共方法expectProperty

--- utils/judgment.ts
+++ utils/judgment.ts
@@ -1,3 +1,26 @@
 export const isNull = (value: any) => value === null;
 export const isAttrFalse = (value: any) =>
   value === null || value === false || value === "false";
+
+/**
+ * 判断 组件属性 用户输入值是否属于 枚举值
+ * @param value 枚举值 数组
+ * @param property 输入的属性值
+ * @param componentName 组件名
+ * @returns
+ */
+export const expectProperty = (
+  value: string[],
+  property: string,
+  componentName: string
+) => {
+  if (value.includes(property)) {
+    return property;
+  } else {
+    const str = value.join("|");
+    console.error(`
+      The ${componentName} property expect ${str}, you are ${property}
+    `);
+    return null;
+  }
+};
