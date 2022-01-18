52acf05d817e83c645e61cf14c67660b712b9dfd
buzhifanji <buzhifanji@163.com>
2022-01-18 15:05:25 +0800

    新增：ARIA 状态属性表

---
+++ aria/aria-statue.ts
@@ -0,0 +1,51 @@
+/***** =============== ARIA 状态属性表 =================== */
+
+/**	字符串。表示检查的状态。
+ * true表示元素被选择；
+ * false表示元素未被选择；
+ * mixed表示元素同时有选择和为选择状态。
+ * */
+export const ariaChecked = "aria-checked";
+
+/**字符串。表禁用状态，
+ * true表示当前是非激活状态；
+ * false表示清除非激活状态。
+ * */
+export const ariaDisabled = "aria-disabled";
+
+/**	字符串。表示展开状态。
+ * 默认为undefined, 表示当前展开状态未知。
+ * 其它可选值：true表示元素是展开的；false表示元素不是展开的。
+ * */
+export const ariaExpanded = "aria-expanded";
+
+/**	字符串。表示展开状态。
+ * 默认为undefined, 表示当前展开状态未知。
+ * 其它可选值：true表示元素是展开的；false表示元素不是展开的。
+ * */
+export const ariaHidden = "aria-hidden";
+
+/**	字符串。表示元素值是否错误的。
+ * 默认为false, 表示是OK的，
+ * 如果为true, 则表示值验证不通过。
+ * */
+export const ariaInvalid = "aria-invalid";
+
+/**	字符串。表示按下的状态，
+ * 可选值有：true, false, mixed, undfined.
+ * 默认为undfined, 表示按下状态未知；
+ * true表示元素往下（按钮按下）；
+ * false表示元素抬起；
+ * mixed表示元素同时有按下和没有按下的状态。
+ * */
+export const ariaPressed = "aria-pressed";
+
+/**
+ * 字符串。表示按下的状态，
+ * 可选值有：true, false, mixed, undfined.
+ * 默认为undfined, 表示按下状态未知；
+ * true表示元素往下（按钮按下）；
+ * false表示元素抬起；
+ * mixed表示元素同时有按下和没有按下的状态。
+ */
+export const ariaSelected = "aria-selected";
