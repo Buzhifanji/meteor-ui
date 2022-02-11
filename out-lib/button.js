761e49a6c832c4bea3f5eb48e1d1c19225d680b4
buzhifanji <buzhifanji@163.com>
2022-02-12 04:34:20 +0800

    删除打包后的代码out-lib

--- out-lib/button.js
+++
@@ -1,3 +0,0 @@
-import './public/button.b5e27daf.js';
-import './public/vendor.71c3293c.js';
-import './public/style.44a3bdb6.js';
--- out-lib/utils.js
+++
@@ -1,1 +0,0 @@
-import"./public/style.44a3bdb6.js";import"./public/vendor.71c3293c.js";
