ae2d9b3df6ec1ce68e2c762580cfdde9bbe6d666
buzhifanji <buzhifanji@163.com>
2022-03-07 00:35:09 +0800

    优化注释

--- src/common/css-variable/name.ts
+++ src/common/css-variable/name.ts
@@ -1,27 +1,32 @@
-export const fontFamilyName = "--meteor-font-family"; // 字体
-
-export const fontSizeName = "--meteor-font-size"; // 字体大小
-
-export const textColorName = "--meteor-text-color"; // 文字颜色
-
-export const widthName = "--meteor-width"; // 宽带
-
-export const heightName = "--meteor-height"; // 高度
-
-export const borderRadiustName = "--meteor-border-radius"; //  边角
-
-export const bezierName = "--meteor-bezier"; //  边角
+/** 字体 */
+export const fontFamilyName = "--meteor-font-family";
+/** 字体大小 */
+export const fontSizeName = "--meteor-font-size";
+/** 文字颜色 */
+export const textColorName = "--meteor-text-color";
+/** 实线颜色 */
+export const dashedColorName = "--meteor-dashed-color";
+/** 背景颜色 */
+export const bgColorName = "--meteor-background-color";
+/** 宽度 */
+export const widthName = "--meteor-width";
+/** 高度 */
+export const heightName = "--meteor-height";
+/** 边角 */
+export const borderRadiustName = "--meteor-border-radius";
+/** 定义三次贝塞尔曲线 */
+export const bezierName = "--meteor-bezier";
 
 /** =================== button ===================== */
-
-export const btnBorderColorName = "--meteor-button-border-color"; // 按钮边框颜色
-
-export const btnBoxShaodowName = "--meteor-button-box-shadow"; // 按钮边阴影
-
-export const btnColorName = "--meteor-button-color"; // 按钮颜色
-
-export const btnBgColorName = "--meteor-button-background-color"; // 按钮背景颜色
-
-export const btnPressedColorName = "--meteor-button-color-pressed"; // 按钮按下去时候的颜色
-
-export const btnPressedBorderColorName = "--meteor-button-border-color-pressed"; // 按钮按下去时候的边框颜色
+/** 按钮边框颜色 */
+export const btnBorderColorName = "--meteor-button-border-color";
+/** 按钮边阴影 */
+export const btnBoxShaodowName = "--meteor-button-box-shadow";
+/** 按钮颜色 */
+export const btnColorName = "--meteor-button-color";
+/** 按钮背景颜色 */
+export const btnBgColorName = "--meteor-button-background-color";
+/** 按钮按下去时候的颜色 */
+export const btnPressedColorName = "--meteor-button-color-pressed";
+/** 按钮按下去时候的边框颜色 */
+export const btnPressedBorderColorName = "--meteor-button-border-color-pressed";
