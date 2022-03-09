249026910e3c8ecf6a5017fee751890b321ca25f
buzhifanji <buzhifanji@163.com>
2022-03-09 17:08:56 +0800

    删除多余代码

--- src/lib/minze-logo.ts
+++
@@ -1,7 +0,0 @@
-import { MinzeElement } from 'minze'
-
-export class MinzeLogo extends MinzeElement {
-  html = () => `
-    <img src="/icon.svg" width="200px" alt="Minze logo">
-  `
-}
--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,3 @@
 export * from "./lib/button/index";
 export * from "./lib/divider/index";
-export * from "./lib/minze-button";
-export * from "./lib/minze-counter";
-export * from "./lib/minze-logo";
 export * from "./lib/text/index";
